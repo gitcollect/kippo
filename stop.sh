330932206893f70ada14f7b564ea1d07798fd786
blubtxt <admin@hamsterbaum.de>
2015-12-27 22:05:06 +0100

    added webUi

--- stop.sh
+++ stop.sh
@@ -10,3 +10,14 @@ if [ -n "$PID" ]; then
   echo "Stopping kippo...\n"
   kill -TERM $PID
 fi
+
+PIDFILE2=http.pid
+
+cd $(dirname $0)
+
+PID=$(cat $PIDFILE2 2>/dev/null)
+
+if [ -n "$PID" ]; then
+  echo "Stopping webUI...\n"
+  kill -TERM $PID
+fi
